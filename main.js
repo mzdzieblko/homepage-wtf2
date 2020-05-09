8562a7ccda69393fe548ad4f4be119d59e7fc5ac
Mateusz Zdziebłko <zdzieblko@icloud.com>
2020-05-09 21:11:09 +0200

    add BEM and empty div

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const age = 31;
 console.log(`Nazwam się ${name},
 mam ${age} lat
 i zaczynam lubić JS`);
+
+const empty = document.querySelector(".empty-js"); // document.querySelector "zapisuje" do zmienne
+
+console.log(empty);
+
+empty.innerHTML = "Test pustego diva";
