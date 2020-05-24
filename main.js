62c119aaee65875c00be5983f6e16d1d80fc809b
Mateusz Zdziebłko <zdzieblko@icloud.com>
2020-05-24 13:59:59 +0200

    trening Lekcja 06.02 JS: Logika

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
-const name = "Mateusz";
-const age = 31;
+// const name = "Mateusz";
+// const age = 31;
 
-console.log(`Nazwam się ${name},
-mam ${age} lat
-i zaczynam lubić JS`);
+// console.log(`Nazwam się ${name},
+// mam ${age} lat
+// i zaczynam lubić JS`);
 
-const empty = document.querySelector(".empty-js"); // document.querySelector "zapisuje" do zmienne
+// const empty = document.querySelector(".empty-js"); // document.querySelector "zapisuje" do zmienne
 
-console.log(empty);
+// console.log(empty);
 
-empty.innerHTML = "Test pustego diva";
+// empty.innerHTML = "Test pustego diva";
+
+const humanOne = {
+  name: 'Maciek',
+  age: 40,
+  address: {
+    street: 'Długa',
+    city: 'Białystok'
+  }
+};
+
+const humanTwo = {
+  name: 'Mateusz',
+  age: 35,
+  address: {
+    street: 'Krótka',
+    city: humanOne.address.city
+  }
+};
+
+console.log(humanOne);
+console.log(humanTwo);
+
+
+if (humanOne.age > humanTwo.age) {
+  console.log("Human one jest starszy");
+} else {
+  console.log("Human two jest starszy");
+}
